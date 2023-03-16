6e0f4a966a34e53ce821bb0e15093a81fce86577
이준용 <iiiuooyoo@gmail.com>
2023-03-16 13:32:11 +0900

    feat : 잼민펀치

---
+++ README.md
@@ -0,0 +1,3 @@
+### Vite + React + GSAP
+
+GSAP에 ScrollTrigger를 사용해봤어요
--- src/App.tsx
+++ src/App.tsx
@@ -5,55 +5,118 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-    const $section = useRef();
+    function punch(text: string) {
+        alert(text);
+    }
 
     useLayoutEffect(() => {
-        gsap.to("#zoomIn", {
+        gsap.to("#zoom-in", {
             scrollTrigger: {
                 trigger: "#section1",
                 start: "top top",
                 end: "bottom center",
                 scrub: true,
             },
-            yPercent: 50,
+            yPercent: -200,
             scale: 3,
             opacity: 0,
         });
 
-        gsap.from("#zoomOut", {
+        gsap.from("#left-punch", {
             scrollTrigger: {
                 trigger: "#section2",
                 start: "top top",
-                end: "bottom center",
-                scrub: true,
+                end: "100",
+                scrub: 1,
             },
-            yPercent: 50,
-            scale: 3,
-            opacity: 0,
+            xPercent: -200,
         });
+
+        gsap.from("#right-punch", {
+            scrollTrigger: {
+                trigger: "#section3",
+                start: "top top",
+                end: "100",
+                scrub: 1,
+            },
+            xPercent: 200,
+        });
+
+        // ScrollTrigger.create({
+        //     trigger: "#fixed",
+        //     start: "top top",
+        //     end: "bottom center",
+        //     endTrigger: "#section2",
+        //     scrub: true,
+        //     onUpdate: (self) => {
+        //         console.log("update", self);
+        //     },
+        // });
 
         gsap.to("#rotate", {
             scrollTrigger: {
-                trigger: "#section3",
-                start: "-200",
-                end: "bottom bottom",
+                trigger: "#section4",
+                start: "top top",
+                end: "bottom center",
                 scrub: true,
             },
             rotate: 360,
             scale: 2,
         });
+
+        gsap.to("#rotate", {
+            scrollTrigger: {
+                trigger: "#section4",
+                start: "-200",
+                end: "200",
+                scrub: true,
+            },
+            rotate: 360,
+            scale: 2,
+        });
+
+        gsap.from("#zoom-out", {
+            scrollTrigger: {
+                trigger: "#section5",
+                start: "-400",
+                end: "bottom center",
+                scrub: true,
+            },
+            yPercent: 200,
+            scale: 3,
+            opacity: 0,
+        });
+
+        ScrollTrigger.refresh();
     }, []);
 
     return (
         <div>
             <section id="section1">
-                <h1 id="zoomIn">어서오고</h1>
+                <h1 id="zoom-in">어서오고</h1>
             </section>
+
             <section id="section2">
-                <h1 id="zoomOut">어서가고</h1>
+                <img
+                    id="left-punch"
+                    src="images/punch_left.jpg"
+                    alt="잼민레프트펀치"
+                    onClick={() => punch("레프트 펀치!")}
+                />
             </section>
             <section id="section3">
+                <img
+                    id="right-punch"
+                    src="images/punch_right.jpg"
+                    alt="잼민펀치라이트펀치"
+                    onClick={() => punch("라이트 펀치!")}
+                />
+            </section>
+            <section id="section4">
                 <h1 id="rotate">돌아가용</h1>
+            </section>
+            <section id="section5">
+                <h1 id="zoom-out">어서가고</h1>
             </section>
         </div>
     );
